e6649afa43fb7f2646e7258846dcb3bbdd1fc770
jtm5356 <jtm5356@rit.edu>
2019-04-19 15:01:45 -0400

    help

--- src/place/client/gui/PlaceGUI.java
+++ src/place/client/gui/PlaceGUI.java
@@ -14,7 +14,7 @@ public class PlaceGUI extends Application implements Observer {
 
     @Override
     public void update(Observable o, Object arg) {
-
+//
     }
 
     public static void main(String[] args) {
