476fa184e3803e9cbbda9677347a5a87ef7862fd
jtm5356 <jtm5356@rit.edu>
2019-04-20 13:01:21 -0400

    basis for GUI

--- src/place/client/ptui/PlacePTUI.java
+++ src/place/client/ptui/PlacePTUI.java
@@ -1,13 +1,21 @@
 package place.client.ptui;
 
+import place.PlaceBoard;
+
 import java.util.Observable;
 import java.util.Observer;
 
 public class PlacePTUI implements Observer {
+
+    // board
+    PlaceBoard board = new PlaceBoard(10);
+    
+
     @Override
     public void update(Observable o, Object arg) {
 
     }
+
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("Usage: java PlaceClient host port username");
