441ab6dded7b4af9745754e2269fbf58234e49da
jtm5356 <jtm5356@rit.edu>
2019-04-29 00:26:33 -0400

    bringing it together

---
+++ src/place/server/ClientThreads.java
@@ -0,0 +1,187 @@
+package place.server;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import place.PlaceBoard;
+import place.PlaceTile;
+import place.network.PlaceRequest;
+
+public class ClientThreads extends Thread implements Closeable {
+
+    private Socket connection;
+    private String clientName;
+    private PlaceBoard board;
+    private Consumer<PlaceTile> tileBeingRecived;
+    private Consumer<String> attemptingLogingIn;
+    private List<Runnable> onDisconnect = new ArrayList<>();
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Instant waitToPlace = Instant.EPOCH;
+    private static int TIMEOUT = 2000;
+
+    public ClientThreads(Socket socket, PlaceBoard board) throws IOException, SocketTimeoutException {
+        this.board = board;
+        this.connection = socket;
+        this.connection.setSoTimeout(TIMEOUT);
+        try {
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+            this.in = new ObjectInputStream(socket.getInputStream());
+        } catch(SocketTimeoutException e) { 
+            socket.close();
+            System.out.printf(
+                "Info: Client with ip address %s failed to complete ObjectStream handshake in time%n",
+                socket.getInetAddress().getHostName()
+            );
+            throw e;
+        }
+
+        this.attemptingLogingIn = (username) -> {
+            System.out.printf("Info: Login attempt from %s with username %s.%n", socket.getInetAddress().getHostName(), username);
+        };
+        this.tileBeingRecived = (tile) -> {
+            System.out.printf("Debug: Tile recieved from client %s: %s%n", getIdentifier(), tile);
+        };
+        this.onDisconnect.add(() -> {
+            System.out.printf("Info: Client %s disconnected.%n", getIdentifier());
+        });
+    }
+
+    public void run() {
+        while(true) {
+            try {
+                PlaceRequest<?> req = (PlaceRequest<?>) in.readUnshared();
+                synchronized(this) {
+                	if(req.getType().equals(PlaceRequest.RequestType.LOGIN)) {
+                		if(clientName == null) {
+                            attemptingLogingIn.accept(""+req.getData());
+                        } else {
+                            sendError("Username already taken please choose another");
+                        }
+                	}
+                	else if(req.getType().equals(PlaceRequest.RequestType.CHANGE_TILE)) {
+                		if(clientName == null) {
+                            sendError("Login process must be complete before you place a tile");
+                            continue;
+                        }
+
+                        Instant now = Instant.now();
+                        if(now.isBefore(waitToPlace)) {
+                            sendError("wait to place another tile for " + waitToPlace);
+                            continue;
+                        }
+                        PlaceTile tile = (PlaceTile) req.getData();
+                        int length = board.getBoard().length;
+                        if(tile.getRow() < 0 || tile.getRow() >= length || tile.getCol() < 0 || tile.getCol() >= length) {
+                            sendError("Tile placement must be in bounds");
+                            continue;
+                        }
+
+                        synchronized(board) {
+                            if(board.getTile(tile.getRow(), tile.getCol()).getColor() == tile.getColor()) {
+                                sendError("Tile sent must differ in color from the existing tile.");
+                                continue;
+                            }
+
+                            tile = new PlaceTile(
+                                tile.getRow(),
+                                tile.getCol(),
+                                clientName,
+                                tile.getColor(),
+                                now.toEpochMilli()
+                            );
+                            board.setTile(tile);
+                        }
+
+                        waitToPlace = now.plusMillis(500);
+                        tileBeingRecived.accept(tile);
+                	}
+                }
+            } catch(SocketTimeoutException e) { 
+                try {
+                    connection.close();
+                } catch(IOException e2) { }
+                System.out.printf("Client " + getIdentifier() + " was too slow to login %n");
+            } catch(IOException e) {
+                try {
+                    connection.close();
+                } catch(IOException e2) { }
+                onDisconnect.forEach(Runnable::run);
+                break;
+            } catch(ClassNotFoundException e) {
+                System.out.printf("Incorrect data recived from client " + getIdentifier());
+            }
+        }
+    }
+
+    public synchronized void tileChange(PlaceTile tile) {
+        try {
+            out.writeUnshared(
+                new PlaceRequest<>(PlaceRequest.RequestType.TILE_CHANGED, tile)
+            );
+        } catch(IOException e) {
+        	System.out.println("error");
+        } 
+    }
+
+    public synchronized void sendError(String msg) {
+        try {
+            out.writeUnshared(
+                new PlaceRequest<>(PlaceRequest.RequestType.ERROR, msg)
+            );
+        } catch(IOException e) {
+        	System.out.println("error");
+        } 
+    }
+
+    public synchronized void usernameAssignments(String username) throws IllegalStateException {
+        if(clientName == null) {
+            clientName = username;
+            try {
+                out.writeUnshared(new PlaceRequest<>(PlaceRequest.RequestType.LOGIN_SUCCESS, username));
+                out.writeUnshared(new PlaceRequest<>(PlaceRequest.RequestType.BOARD, board));
+                connection.setSoTimeout(0); 
+            } catch(IOException e) {} 
+        } else {
+            throw new IllegalStateException("Username has already been taken");
+        }
+    }
+    public synchronized void registerOnLoginAttempt(Consumer<String> loginAttempt) {
+        attemptingLogingIn = attemptingLogingIn.andThen(loginAttempt);
+    }
+    public synchronized void registerOnTileReceived(Consumer<PlaceTile> tileReceived) {
+        tileBeingRecived = tileBeingRecived.andThen(tileReceived);
+    }
+
+    public synchronized void registerOnDisconnect(Runnable disconnect) {
+        onDisconnect.add(disconnect);
+    }
+
+    public String getIdentifier() {
+        if(clientName == null) {
+            return "with ip address " + connection.getInetAddress().getHostName();
+        } else {
+            return "with username " + clientName;
+        }
+    }
+
+    public String getUsername() {
+        return clientName;
+    }
+
+    public Socket getSocket() {
+        return connection;
+    }
+
+    public void close() throws IOException {
+        connection.close();
+    }
+}
--- src/place/server/PlaceServer.java
+++ src/place/server/PlaceServer.java
@@ -1,32 +1,150 @@
 package place.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import place.PlaceBoard;
+import place.PlaceTile;
 
-public class PlaceServer {
+public class PlaceServer extends Thread {
 
-    public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            System.err.println("Usage: java ReversiServer <port> <DIM>");
-            System.exit(1);
+    private PlaceBoard board;
+    private Map<InetAddress, ClientThreads> clientList = new HashMap<>();
+    private Set<InetAddress> clientsWaiting = new HashSet<>();
+    private ServerSocket primarySocket;
+    private int port;
+
+    public PlaceServer(int port, int size) {
+        this.port = port;
+        this.board = new PlaceBoard(size);
+    }
+    public void run() {
+        try {
+            primarySocket = new ServerSocket(port);
+            while(true) {
+                Socket connection = primarySocket.accept();
+
+                new Thread(() -> {
+
+                    InetAddress address = connection.getInetAddress();
+                    synchronized(clientsWaiting) {
+                        if(clientsWaiting.contains(address)) {
+                            System.out.printf("Info: Rejecting connection " +
+                                    "from %s due to pre-existing pending " +
+                                    "connection.%n", address.getHostName());
+                            try {
+                                connection.close();
+                            } catch(IOException e) {
+                            	System.out.println("error");
+                            }
+                            return;
+                        }
+                        else {
+                            clientsWaiting.add(address);
+                        }
+                    }
+
+                    ClientThreads client;
+                    try {
+                        client = new ClientThreads(connection, board);
+
+                        ClientThreads oldConnection = clientList.put(address, client);
+                        if(oldConnection != null) {
+                            oldConnection.close();
+                        }
+                    } catch (SocketTimeoutException e) { // Thrown if setting up the connection times out.
+                    	System.out.println("Timed out");
+                    	return;
+                    } catch (java.io.StreamCorruptedException e) {
+                        System.out.println("Corrupted stream");
+                        return;
+                    } catch(IOException e) {
+                        System.out.println( "error - " + e);
+                        return;
+                    } finally {
+                        synchronized(clientsWaiting) {
+                            clientsWaiting.remove(address);
+                        }
+                    }
+                    synchronized(this) {
+                        System.out.println(clientList.size());
+                        if (clientList.size() > (board.DIM/4)) {
+                            System.out.printf("Too many people are connected "
+                            		+ "please try again " + address.getHostName());
+                            client.sendError("Too many people were connected to " +
+                                    "Please try connecting ");
+                            removeClient(client);
+                            try {
+                                connection.close();
+                            } catch (IOException e) {
+                            	System.out.println( "error - " + e);
+                            }
+                            return;
+                        }
+                    }
+
+                    client.registerOnLoginAttempt(uname -> loginClient(client, uname));
+                    client.registerOnTileReceived(this::sendToAll);
+                    client.registerOnDisconnect(() -> removeClient(client));
+
+                    System.out.printf("Info: Client %s connected.%n", client.getIdentifier());
+                    client.start();
+
+                }).start();
+            }
+        } catch (SecurityException e) {
+            System.err.println("Failed to bind to port: permision denied");
+            System.err.println(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        int dim = Integer.parseInt(args[0]);
-        ServerSocket ss=new ServerSocket(Integer.parseInt(args[1]));
-        System.out.println("waiting for Player one ...");
-        int counter = 0;
-        PlaceBoard board = new PlaceBoard(dim);
-        while (true) {
-            counter++;
-            Socket serverClient = ss.accept();
-            System.out.println("Client No:" + counter + " started!");
-            SCT sct = new SCT(serverClient, counter, board);
-            sct.start();
+    }
+    public static void main(String[] args) {
+        if(args.length <2) {
+            System.out.println("Usage: java PlaceServer <port> <size>/n Not enough arguments");
+            System.exit(-1);
+        } else {
+            try {
+                new PlaceServer(
+                    Integer.parseInt(args[0]),
+                    Integer.parseInt(args[1])
+                ).start();
+            } catch (NumberFormatException e) {
+                System.out.println("All arguments must be integers.");
+                System.exit(-1);
+            }
         }
+    }
+    
+    private void loginClient(ClientThreads client, String requestedUsername) {
+    	int ID = 0;
+        String username = requestedUsername;
+        HashSet<String> usernames;
+        synchronized(this) {
+            usernames = clientList.values()
+                                    .stream()
+                                    .map(ClientThreads::getUsername)
+                                    .collect(Collectors.toCollection(HashSet::new));
+        }
+        while(usernames.contains(username)) {
+            username = requestedUsername + ++ID;
+        }
+        client.usernameAssignments(username);
+    }
+    
+    private void sendToAll(PlaceTile tile) {
+        clientList.values().stream().parallel().forEach(c -> c.tileChange(tile));
+    }
+    
+    private void removeClient(ClientThreads client) {
+        clientList.remove(client.getSocket().getInetAddress(), client);
     }
 }
